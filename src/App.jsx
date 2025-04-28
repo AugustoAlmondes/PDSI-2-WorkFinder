c8ff574b5b88f9e5f7d1d7ab8cdf39de079d14a9
WanderleiSI <jose.rocha@ufpi.edu.br>
2025-04-28 11:09:52 -0300

    feat: Ajustes componentes principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-// import { useState } from 'react';
-// import Login from './pages/Login/Login';  // Comentando a importação do Login
-
-import './App.css';  // Estilos globais
-
-import Cadastro from './pages/Cadastro/Cadastro.jsx'; // Caminho correto
-
-import InfoJob from './pages/InfoJob/InfoJob.jsx';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 
 function App() {
 
-    // const [fezLogin, setFezLogin] = useState(false); //True para logado e falso para deslogado
-    // const [typeUser, setTypeUser] = useState(1); //0 para empresa, 1 para usuário, 2 para adm
+    const [fezLogin, setFezLogin] = useState(false); //True para logado e falso para deslogado
+    const [typeUser, setTypeUser] = useState(1); //0 para empresa, 1 para usuário, 2 para adm
 
     return (
         <>
-            {/* <Login /> */}  
-            
-            {/* <Cadastro /> */}  
-            <InfoJob/>  
+            {/* <Login setFezLogin={setFezLogin} setTypeUser={setTypeUser} /> */}
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home fezLogin={fezLogin} typeUser={typeUser} />} />
+                    <Route path="/login" element={<Login setFezLogin={setFezLogin} setTypeUser={setTypeUser} />} />
+                </Routes>
+            </Router>
         </>
     );
 }
